a35bbb45f522a9427111ef387e3c1182661eb726
Derek Liu <derek.liu@utoronto.ca>
2020-03-29 19:02:07 -0400

    working version

--- VMTranslator.py
+++ VMTranslator.py
@@ -1,12 +1,10 @@
-import pathlib
 import os
-import re
 import sys
 
 
 def trim(docstring):
     if not docstring:
-        return ''
+        return ""
     # Convert tabs to spaces (following the normal Python rules)
     # and split into a list of lines:
     lines = docstring.expandtabs().splitlines()
@@ -27,42 +25,47 @@ def trim(docstring):
     while trimmed and not trimmed[0]:
         trimmed.pop(0)
     # Return a single string:
-    return '\n'.join(trimmed)
+    return "\n".join(trimmed)
 
 
 class Parser(object):
     def __init__(self, inputfile):
-        cwd = os.getcwd()
-        self.file = open(pathlib.Path(cwd, inputfile))
+        self.file = open(inputfile)
         self.currentCommand = ""
         self.hasMoreCommands = True
 
     def advance(self):
-        try: 
+        try:
             while True:
-                self.currentCommand = re.sub(r"\s+.*", "", self.file.readline().lstrip())
-                if self.currentCommand == '':
+                self.currentCommand = self.file.readline()
+                if self.currentCommand == "":
                     raise EOFError
-                if not self.currentCommand.startswith(("\n", r"\\")):
+                if not self.currentCommand.startswith((r"//", "\n")):
                     break
         except EOFError:
             self.hasMoreCommands = False
 
-    def commandtype(self):
-        a = self.arg1()
-        if a == "push":
+    def commandtype(self, command):
+        if command.startswith("push"):
             return "C_PUSH"
-        elif a == "pop":
+        elif command.startswith("pop"):
             return "C_POP"
-        elif a in ("add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"):
+        elif command.startswith(
+            ("add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not")
+        ):
             return "C_ARITHMETIC"
 
     def arg1(self):
-        return self.currentCommand.split(" ")[0]
+        return self.currentCommand.split(" ")[0].replace("\n", "")
 
     def arg2(self):
-        if self.commandtype() in ("C_PUSH", "C_POP", "C_FUNCTION", "C_CALL"):
-            return self.currentCommand.split(" ")[1:]
+        if self.commandtype(self.currentCommand) in (
+            "C_PUSH",
+            "C_POP",
+            "C_FUNCTION",
+            "C_CALL",
+        ):
+            return [i.replace("\n", "") for i in self.currentCommand.split(" ")[1:]]
 
     def close(self):
         self.file.close()
@@ -70,10 +73,9 @@ class Parser(object):
 
 class CodeWriter(object):
     def __init__(self, outputfile):
-        cwd = os.getcwd()
-        self.file = open(pathlib.Path(cwd, outputfile), "w")
+        self.file = open(outputfile, "w")
         self.labelcounter = 0
-        self.filename = outputfile.replace('.asm','')
+        self.filename = os.path.basename(outputfile).replace(".asm", "")
 
     def writeArithmetic(self, command):
         if command == "add":
@@ -82,13 +84,17 @@ class CodeWriter(object):
             @SP
             M=M-1
             @SP
+            A=M
             D=M
             @SP
             M=M-1
             @SP
-            MD=D+M
+            A=M
+            M=D+M
+            @SP
+            M=M+1
             """
-            self.file.writelines(trim(asm).format('\n' + r'\\ ' + command))
+            self.file.writelines(trim(asm).format("\n" + r"// " + command))
 
         elif command == "sub":
             asm = """
@@ -96,13 +102,17 @@ class CodeWriter(object):
             @SP
             M=M-1
             @SP
+            A=M
             D=M
             @SP
             M=M-1
             @SP
-            MD=M-D
+            A=M
+            M=M-D
+            @SP
+            M=M+1
             """
-            self.file.writelines(trim(asm).format('\n' + r'\\ ' + command))
+            self.file.writelines(trim(asm).format("\n" + r"// " + command))
 
         elif command == "neg":
             asm = """
@@ -110,9 +120,12 @@ class CodeWriter(object):
             @SP
             M=M-1
             @SP
-            M=-D
+            A=M
+            M=-M
+            @SP
+            M=M+1
             """
-            self.file.writelines(trim(asm).format('\n' + r'\\ ' + command))
+            self.file.writelines(trim(asm).format("\n" + r"// " + command))
 
         elif command == "eq":
             asm = """
@@ -120,28 +133,34 @@ class CodeWriter(object):
             @SP
             M=M-1
             @SP
+            A=M
             D=M
             @SP
             M=M-1
             @SP
+            A=M
             D=M-D
-            @EQ##{1}
+            @EQ{1}
             D;JEQ
-            @NEQ##{2}
+            @NEQ{2}
             0;JMP
-            (EQ##{1})
+            (EQ{1})
             @SP
+            A=M
             M=-1
-            @END##{3}
+            @END{3}
             0;JMP    
-            (NEQ##{2})
+            (NEQ{2})
             @SP
+            A=M
             M=0
-            (END##{3})
+            (END{3})
+            @SP
+            M=M+1
             """
             labeln = [i for i in range(self.labelcounter, self.labelcounter + 3)]
             self.labelcounter += 3
-            self.file.writelines(trim(asm).format('\n' + r'\\ ' + command, *labeln))
+            self.file.writelines(trim(asm).format("\n" + r"// " + command, *labeln))
 
         elif command == "gt":
             asm = """
@@ -149,28 +168,34 @@ class CodeWriter(object):
             @SP
             M=M-1
             @SP
+            A=M
             D=M
             @SP
             M=M-1
             @SP
+            A=M
             D=M-D
-            @GT##{1}
+            @GT{1}
             D;JGT
-            @NGT##{2}
+            @NGT{2}
             0;JMP
-            (GT##{1})
+            (GT{1})
             @SP
+            A=M
             M=-1
-            @END##{3}
+            @END{3}
             0;JMP    
-            (NGT##{2})
+            (NGT{2})
             @SP
+            A=M
             M=0
-            (END##{3})
+            (END{3})
+            @SP
+            M=M+1
             """
             labeln = [i for i in range(self.labelcounter, self.labelcounter + 3)]
             self.labelcounter += 3
-            self.file.writelines(trim(asm).format('\n' + r'\\ ' + command, *labeln))
+            self.file.writelines(trim(asm).format("\n" + r"// " + command, *labeln))
 
         elif command == "lt":
             asm = """
@@ -178,28 +203,34 @@ class CodeWriter(object):
             @SP
             M=M-1
             @SP
+            A=M
             D=M
             @SP
             M=M-1
             @SP
+            A=M
             D=M-D
-            @LT##{1}
+            @LT{1}
             D;JLT
-            @NLT##{2}
+            @NLT{2}
             0;JMP
-            (LT##{1})
+            (LT{1})
             @SP
+            A=M
             M=-1
-            @END##{3}
+            @END{3}
             0;JMP    
-            (NLT##{2})
+            (NLT{2})
             @SP
+            A=M
             M=0
-            (END##{3})
+            (END{3})
+            @SP
+            M=M+1
             """
             labeln = [i for i in range(self.labelcounter, self.labelcounter + 3)]
             self.labelcounter += 3
-            self.file.writelines(trim(asm).format('\n' + r'\\ ' + command, *labeln))
+            self.file.writelines(trim(asm).format("\n" + r"// " + command, *labeln))
 
         elif command == "and":
             asm = """
@@ -207,13 +238,17 @@ class CodeWriter(object):
             @SP
             M=M-1
             @SP
+            A=M
             D=M
             @SP
             M=M-1
             @SP
-            MD=D&M
+            A=M
+            M=D&M
+            @SP
+            M=M+1
             """
-            self.file.writelines(trim(asm).format('\n' + r'\\ ' + command))
+            self.file.writelines(trim(asm).format("\n" + r"// " + command))
 
         elif command == "or":
             asm = """
@@ -221,13 +256,17 @@ class CodeWriter(object):
             @SP
             M=M-1
             @SP
+            A=M
             D=M
             @SP
             M=M-1
             @SP
+            A=M
             MD=D|M
+            @SP
+            M=M+1
             """
-            self.file.writelines(trim(asm).format('\n' + r'\\ ' + command))
+            self.file.writelines(trim(asm).format("\n" + r"// " + command))
 
         elif command == "not":
             asm = """
@@ -235,32 +274,32 @@ class CodeWriter(object):
             @SP
             M=M-1
             @SP
-            MD=!M
+            A=M
+            M=!M
+            @SP
+            M=M+1
             """
-            self.file.writelines(trim(asm).format('\n' + r'\\ ' + command))
+            self.file.writelines(trim(asm).format("\n" + r"// " + command))
 
     def close(self):
         self.file.close()
 
     def writepushpop(self, command, segment, index):
         # build command string, same for all segments
+        commandstring = "{}// {} {} {}".format("\n", command, segment, index)
 
-        commandstring = "{}// {} {} {}".format('\n', command, segment, index)
+        # translate segment into assembly segment name
+        if segment == "local":
+            segmentName = "LCL"
+        elif segment == "argument":
+            segmentName = "ARG"
+        elif segment == "this":
+            segmentName = "THIS"
+        elif segment == "that":
+            segmentName = "THAT"
 
-        #translate segment into assembly segment name
-        if segment == 'local':
-            segmentName = 'LCL'
-        elif segment == 'argument':
-            segmentName = 'ARG'
-        elif segment == 'this':
-            segmentName = 'THIS'
-        elif segment == 'THAT':
-            segmentName = 'THAT'
-        elif segment == 'temp':
-            segmentName = '5'
-        
-        if command == 'push':
-            if segment == 'constant':
+        if command == "push":
+            if segment == "constant":
                 asm = """
                 {}
                 @{}
@@ -272,16 +311,16 @@ class CodeWriter(object):
                 M=M+1
                 """
                 self.file.writelines(trim(asm).format(commandstring, index))
-            elif segment in ('local','argument','this','that','temp'):
+            elif segment in ("local", "argument", "this", "that"):
                 asm = """
                 {0}
                 @{1}
                 D=M
                 @{2}
                 D=D+A
-                @addr##{3}
+                @addr{3}
                 M=D
-                @addr##{3}
+                @addr{3}
                 A=M
                 D=M
                 @SP
@@ -290,9 +329,26 @@ class CodeWriter(object):
                 @SP
                 M=M+1
                 """
-                self.file.writelines(trim(asm).format(commandstring, segmentName, index, self.labelcounter))
+                self.file.writelines(
+                    trim(asm).format(
+                        commandstring, segmentName, index, self.labelcounter
+                    )
+                )
                 self.labelcounter += 1
-            elif segment == 'static':
+            elif segment == "temp":
+                segmentName = str(5 + int(index))
+                asm = """
+                {}
+                @{}
+                D=M
+                @SP
+                A=M
+                M=D
+                @SP
+                M=M+1
+                """
+                self.file.writelines(trim(asm).format(commandstring, segmentName))
+            elif segment == "static":
                 segmentName = "{}.{}".format(self.filename, index)
                 asm = """
                 {}
@@ -305,11 +361,11 @@ class CodeWriter(object):
                 M=M+1
                 """
                 self.file.writelines(trim(asm).format(commandstring, segmentName))
-            elif segment == 'pointer':
-                if index == 0:
-                    segmentName = 'THIS'
+            elif segment == "pointer":
+                if index == "0":
+                    segmentName = "THIS"
                 else:
-                    segmentName = 'THAT'
+                    segmentName = "THAT"
                 asm = """
                 {}
                 @{}
@@ -322,28 +378,47 @@ class CodeWriter(object):
                 """
                 self.file.writelines(trim(asm).format(commandstring, segmentName))
 
-        elif command == 'pop':
-            if segment in ('local', 'argument', 'this', 'that', 'temp'):
+        elif command == "pop":
+            if segment in ("local", "argument", "this", "that"):
                 asm = """
                 {0}
                 @{1}
                 D=M
                 @{2}
                 D=D+A
-                @addr##{3}
+                @addr{3}
                 M=D
                 @SP
                 M=M-1
                 @SP
                 A=M
                 D=M
-                @addr##{3}
+                @addr{3}
                 A=M
                 M=D
                 """
-                self.file.writelines(trim(asm).format(commandstring, segmentName, index, self.labelcounter))
+                self.file.writelines(
+                    trim(asm).format(
+                        commandstring, segmentName, index, self.labelcounter
+                    )
+                )
                 self.labelcounter += 1
-            elif segment == 'static':
+            elif segment == "temp":
+                segmentName = str(5 + int(index))
+                asm = """
+                {0}
+                @SP
+                M=M-1
+                @SP
+                A=M
+                D=M
+                @{1}
+                A=M
+                M=D
+                """
+                self.file.writelines(trim(asm).format(commandstring, segmentName))
+
+            elif segment == "static":
                 segmentName = "{}.{}".format(self.filename, index)
                 asm = """
                 {}
@@ -356,11 +431,11 @@ class CodeWriter(object):
                 M=D
                 """
                 self.file.writelines(trim(asm).format(commandstring, segmentName))
-            elif segment == 'pointer':
-                if index == 0:
-                    segmentName = 'THIS'
+            elif segment == "pointer":
+                if index == "0":
+                    segmentName = "THIS"
                 else:
-                    segmentName = 'THAT'
+                    segmentName = "THAT"
                 asm = """
                 {}
                 @SP
@@ -373,16 +448,21 @@ class CodeWriter(object):
                 """
                 self.file.writelines(trim(asm).format(commandstring, segmentName))
 
-                
+
 if __name__ == "__main__":
     inputf = sys.argv[1]
     outputfile = os.path.basename(inputf).replace(".vm", ".asm")
-    outputfullname = os.path.join("tests", outputfile)
+    outputpath = os.path.split(inputf)[0]
+    outputfullname = os.path.join(outputpath, outputfile)
     parser = Parser(inputf)
     cw = CodeWriter(outputfullname)
     while parser.hasMoreCommands:
         parser.advance()
-        cw.writeArithmetic(parser.currentCommand)
+        if parser.commandtype(parser.currentCommand) == "C_ARITHMETIC":
+            cw.writeArithmetic(parser.arg1())
+        elif parser.commandtype(parser.currentCommand) in ("C_PUSH", "C_POP"):
+            cmd = parser.arg1()
+            seg, idx = parser.arg2()
+            cw.writepushpop(cmd, seg, idx)
     cw.close()
     parser.close()
-

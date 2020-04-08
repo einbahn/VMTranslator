714df4a9e17ecc16ec34d089466eab14211efe2e
Derek Liu <derek@derekliu.me>
2020-04-08 16:26:48 -0400

    wrong return implementation

--- VMTranslator.py
+++ VMTranslator.py
@@ -54,18 +54,37 @@ class Parser(object):
             ("add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not")
         ):
             return "C_ARITHMETIC"
+        elif command.startswith("label"):
+            return "C_LABEL"
+        elif command.startswith("goto"):
+            return "C_GOTO"
+        elif command.startswith("if-goto"):
+            return "C_IF"
+        elif command.startswith("call"):
+            return "C_CALL"
+        elif command.startswith("function"):
+            return "C_FUNCTION"
+        elif command.startswith("return"):
+            return "C_RETURN"
 
-    def arg1(self):
-        return self.currentCommand.split(" ")[0].replace("\n", "")
+    def arg1(self, commandtype):
+        if commandtype == 'C_RETURN':
+            raise ValueError
+        if commandtype == 'C_ARITHMETIC':
+            return self.currentCommand.split(" ")[0].replace("\n", "")
+        else:
+            return self.currentCommand.split(' ')[1].replace('\n', '')
 
-    def arg2(self):
-        if self.commandtype(self.currentCommand) in (
+    def arg2(self, commandtype):
+        if commandtype in (
             "C_PUSH",
             "C_POP",
             "C_FUNCTION",
             "C_CALL",
         ):
-            return [i.replace("\n", "") for i in self.currentCommand.split(" ")[1:]]
+            return self.currentCommand.split(' ')[2].replace('\n', '')
+        else:
+            raise ValueError
 
     def close(self):
         self.file.close()
@@ -284,7 +303,12 @@ class CodeWriter(object):
     def close(self):
         self.file.close()
 
-    def writepushpop(self, command, segment, index):
+    def writepushpop(self, commandtype, segment, index):
+        if commandtype == 'C_PUSH':
+            command = 'push'
+        else:
+            command = 'pop'
+
         # build command string, same for all segments
         commandstring = "{}// {} {} {}".format("\n", command, segment, index)
 
@@ -447,7 +471,190 @@ class CodeWriter(object):
                 M=D
                 """
                 self.file.writelines(trim(asm).format(commandstring, segmentName))
+        
+    def writelabel(self, label):
+        asm = """
+        {0}// label {1}
+        ({1})
+        """
+        self.file.writelines(trim(asm).format('\n', label))
+    
+    def writegoto(self, label):
+        asm = """
+        {0}//goto {1}
+        @{1}
+        0;jmp
+        """
+        self.file.writelines(trim(asm).format('\n', label))
 
+    def writeif(self, label):
+        asm = """
+        {0}//if-goto {1}
+        @SP
+        M=M-1
+        @SP
+        A=M
+        D=M
+        @{1}
+        D;JNE
+        """
+        self.file.writelines(trim(asm).format('\n', label))
+
+    def writecall(self, functionname, nargs):
+        asm = """
+        {3}// call {0} {1}
+        ({0}.retaddr{2}) // declare label
+        @{0}.retaddr{2} // push returnAddress
+        D=A
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1
+        @LCL        // push LCL
+        D=A
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1
+        @ARG        // push ARG
+        D=A
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1 
+        @THIS       // push THIS
+        D=A
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1
+        @THAT       // push THAT
+        D=A
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1
+        @SP         // arg = SP - 5 - nArgs
+        D=A
+        @5
+        D=D-A
+        @{1}
+        D=D-A
+        @ARG
+        M=D
+        @SP         // LCL = SP
+        D=A
+        @LCL
+        M=D
+        @{0}        // goto {0} transfers control to the called function
+        0;JMP
+        ({0}.retaddr{2})  // declares a label for the return address
+        """
+        self.file.writelines(trim(asm).format(
+            functionname,
+            nargs,
+            self.labelcounter,
+            '\n'
+            )
+        )
+        self.labelcounter += 1
+
+    def writefunction(self, functionname, numlocals):
+        asm = """{3}// function {0} {1}
+        ({0})
+        @i{2}
+        M=0
+        (LOOP{2})
+        @i{2}       // push 0 * numlocals
+        D=M
+        @{1}
+        D=D-A
+        @END{2}
+        D;JGE
+        @LCL
+        D=M
+        @i{2}
+        A=D+M
+        M=0
+        @i{2}
+        M=M+1
+        @SP
+        M=M+1
+        @LOOP{2}
+        0;JMP
+        (END{2})
+        """
+        self.file.writelines(trim(asm).format(
+            functionname,
+            numlocals,
+            self.labelcounter,
+            '\n'
+            )
+        )
+        self.labelcounter += 1
+
+    def writereturn(self):
+        asm = """
+        {1}// return
+        @LCL            //endframe = LCL
+        D=A
+        @endFrame{0}
+        M=D
+        @endFrame{0}    // retAddr = *(endFrame - 5)
+        D=M
+        @5
+        D=D-A
+        @retAddr{0}
+        M=D
+        @SP             // *ARG = POP
+        M=M-1
+        @SP
+        A=M
+        D=M
+        @ARG
+        A=M
+        M=D
+        @ARG            // SP = ARG + 1
+        D=A+1
+        @SP
+        A=M
+        M=D
+        @endFrame{0}    // THAT = *(endFrame - 1)
+        D=M-1
+        @THAT
+        A=M
+        M=D
+        @endFrame{0}    // THIS = *(endFrame - 2)
+        D=M
+        @2
+        D=D-A
+        @THIS
+        A=M
+        M=D
+        @endFrame{0}    // ARG = *(endFrame - 3)
+        D=M
+        @3
+        D=D-A
+        @ARG
+        A=M
+        M=D
+        @endFrame{0}    // LCL = *(endFrame - 4)
+        D=M
+        @4
+        D=D-A
+        @LCL
+        A=M
+        M=D
+        @retAddr{0}     // goto retAddr
+        0;JMP
+        """
+        self.file.writelines(trim(asm).format(self.labelcounter, '\n'))
+        self.labelcounter += 1
 
 if __name__ == "__main__":
     inputf = sys.argv[1]
@@ -458,11 +665,32 @@ if __name__ == "__main__":
     cw = CodeWriter(outputfullname)
     while parser.hasMoreCommands:
         parser.advance()
-        if parser.commandtype(parser.currentCommand) == "C_ARITHMETIC":
-            cw.writeArithmetic(parser.arg1())
-        elif parser.commandtype(parser.currentCommand) in ("C_PUSH", "C_POP"):
-            cmd = parser.arg1()
-            seg, idx = parser.arg2()
-            cw.writepushpop(cmd, seg, idx)
+        currcmd = parser.currentCommand
+        cmdtype = parser.commandtype(currcmd)
+        if cmdtype == "C_ARITHMETIC":
+            cw.writeArithmetic(parser.arg1(cmdtype))
+        elif cmdtype in ("C_PUSH", "C_POP"):
+            seg = parser.arg1(cmdtype)
+            idx = parser.arg2(cmdtype)
+            cw.writepushpop(cmdtype, seg, idx)
+        elif cmdtype in ('C_LABEL', 'C_GOTO', 'C_IF'):
+            label = parser.arg1(cmdtype)
+            if cmdtype == 'C_LABEL':
+                cw.writelabel(label)
+            elif cmdtype == 'C_GOTO':
+                cw.writegoto(label)
+            elif cmdtype == 'C_IF':
+                cw.writeif(label)
+        elif cmdtype == 'C_FUNCTION':
+            functionname = parser.arg1(cmdtype)
+            nlocals = parser.arg2(cmdtype)
+            cw.writefunction(functionname, nlocals)
+        elif cmdtype == 'C_CALL':
+            functionname = parser.arg1(cmdtype)
+            nargs = parser.arg2(cmdtype)
+            cw.writecall(functionname, nargs)
+        elif cmdtype == 'C_RETURN':
+            cw.writereturn()
+
     cw.close()
     parser.close()
